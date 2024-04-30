a7fb5dc55adfaf8dcd7d72fc9c77f6beeba0e0b0
Adam Jones <domdomegg+git@gmail.com>
2024-04-30 02:10:17 +0100

    Fix urlToTest

--- src/urlToTest.ts
+++ src/urlToTest.ts
@@ -1,1 +1,1 @@
-export const url = process.env.URL_TO_TEST ?? 'http://localhost:8080/';
+export const url = (typeof __ENV !== 'undefined' ? __ENV.URL_TO_TEST : process.env.URL_TO_TEST) ?? 'http://localhost:8080/';
